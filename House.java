9456ea574b1b3b8d6968b19518e73a7e523b49ce
Emkung <ekung@smith.edu>
2023-04-01 17:30:04 -0400

    extension reason

--- House.java
+++ House.java
@@ -1,12 +1,57 @@
 /* This is a stub for the House class */
-public class House {
-
-  public House() {
-    System.out.println("You have built a house: 🏠");
+import java.util.ArrayList;
+public class House extends Building{
+  private ArrayList<String> residents;
+  private boolean hasDiningRoom;
+  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+    // System.out.println("You have built a house: 🏠");
+    super(name, address, nFloors);
+    this.residents = new ArrayList<String>();
+    this.hasDiningRoom = hasDiningRoom;
   }
 
-  public static void main(String[] args) {
-    new House();
+    /** Accessor for hasDiningRoom */
+    public boolean hasDiningRoom() {
+      return this.hasDiningRoom;
+    }
+  
+    /** Accessor for number of residents */
+    public int nResidents() {
+      return this.residents.size();
+    }
+  
+    public void moveIn(String name) {
+      // check if this.residents contains name
+      if (this.residents.contains(name)) {
+        //   if so: throw and exception
+        throw new RuntimeException(name + " is already a resident of " + this.getName());
+      }
+      // if we're good to go, add to roster
+      this.residents.add(name);
+      System.out.println(name + " has just moved into " + this.getName() + "! Go say hello :-)");
+    }
+  
+    public String toString() {
+      String description = super.toString();
+      description += " There are currently " + this.nResidents() + " people living in this house.";
+      description += " This house ";
+      if (this.hasDiningRoom) {
+        description += "has";
+      } else {
+        description += "does not have";
+      }
+      description += " an active dining room.";
+      return description;
+    }
+  
+    public static void main(String[] args) {
+      House morrow = new House("Morrow", "The Quad", 4, false);
+      System.out.println(morrow);
+      morrow.moveIn("Jordan");
+      morrow.moveIn("Jordan");
+      System.out.println(morrow);
+      House king = new House("King", "The Quad", 3, true);
+      System.out.println(king);
+    }
+  
   }
-
-}
--- Library.java
+++ Library.java
@@ -1,12 +1,36 @@
+import java.util.Hashtable;
 /* This is a stub for the Library class */
-public class Library {
+public class Library extends Building{
+  private Hashtable<String, Boolean> collection;
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
+    public Library(String name, String address, int nFloors, Hashtable collection) {
+      super(name, address, nFloors);
+      this.collection = new Hashtable<String, Boolean>();
+      // System.out.println("You have built a library: 📖");
+    }
+    public void addTitle(String title){
+      this.collection.put(title, true);
+
+    }
+    public String removeTitle(String title){
+      this.collection.remove(title);
+      return title;
+
+    } 
+    public void checkOut(String title){
+      this.collection
+    }
+    public void returnBook(String title){
+
+    }
+    public boolean containsTitle(String title){ // returns true if the title appears as a key in the Libary's collection, false otherwise
+    }
+    public boolean isAvailable(String title){// returns true if the title is currently available, false otherwise
+    }
+    public void printCollection(){// prints out the entire collection in an easy-to-read way (including checkout status)
     }
   
     public static void main(String[] args) {
-      new Library();
     }
   
   }
